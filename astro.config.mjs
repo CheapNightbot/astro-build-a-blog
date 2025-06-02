d254ded99239e778d584948140509c3f4b924fb7
Cheap Nightbot <151442433+CheapNightbot@users.noreply.github.com>
2025-06-02 12:08:11 +0900

    Update astro.config.mjs

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,7 +1,10 @@
 // @ts-check
 import { defineConfig } from 'astro/config';
 
+import preact from "@astrojs/preact";
+
 // https://astro.build/config
 export default defineConfig({
-    site: "https://mellow-zuccutto-c04d24.netlify.app"
+  site: "https://mellow-zuccutto-c04d24.netlify.app",
+  integrations: [preact()]
 });
